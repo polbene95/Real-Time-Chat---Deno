3de53e49f2ba14c34f775c7dce5b1aa0d2cd1d1d
Pol Benedito Momplet <polbene@gmail.com>
2020-06-23 17:26:38 +0200

    heroku deploy closed

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 import { listenAndServe } from 'https://deno.land/std/http/server.ts';
-import * as flags from 'https://deno.land/std/flags/mod.ts';
+// import * as flags from 'https://deno.land/std/flags/mod.ts';
 import { acceptWebSocket, acceptable } from 'https://deno.land/std/ws/mod.ts';
-import { Application } from "https://deno.land/x/oak/mod.ts";
+// import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import chat from './chat.js';
 
-const { args, exit } = Deno;
-const DEFAULT_PORT = 3000;
-const argPort = flags.parse(args).port;
-const port = argPort ? Number(argPort) : DEFAULT_PORT;
+// const { args, exit } = Deno;
+// const DEFAULT_PORT = 3000;
+// const argPort = flags.parse(args).port;
+// const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-if (isNaN(port)) {
-    console.log('port is not a number');
+// if (isNaN(port)) {
+//     console.log('port is not a number');
     
-    exit(1)
-} 
+//     exit(1)
+// } 
 
 
 
-const app = new Application();
+// const app = new Application();
 
 
-app.use(async (context) => {
-    await send(context, context.request.url.pathname, {
-      root: `${Deno.cwd()}/public`,
-      index: "chat.html",
-    });
-});
+// app.use(async (context) => {
+//     await send(context, context.request.url.pathname, {
+//       root: `${Deno.cwd()}/public`,
+//       index: "chat.html",
+//     });
+// });
 
 
 listenAndServe({port}, async (request)=>{
@@ -38,7 +38,7 @@ listenAndServe({port}, async (request)=>{
             headers: request.headers
         })
         .then(chat);
-    app.listen({port})
+    // app.listen({port})
 })
 
 console.log(`Server started on port 3000`);
