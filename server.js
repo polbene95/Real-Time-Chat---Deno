766aa513bafaba936def9ab3dd470c51ca0611b8
Pol Benedito Momplet <polbene@gmail.com>
2020-06-23 17:24:40 +0200

    oak added

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ listenAndServe({port}, async (request)=>{
             headers: request.headers
         })
         .then(chat);
+    app.listen({port})
 })
 
 console.log(`Server started on port 3000`);
