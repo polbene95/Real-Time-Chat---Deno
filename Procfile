b884c9e3e561959eb04f2c71859078b899faa4bb
Pol Benedito Momplet <polbene@gmail.com>
2020-07-01 14:10:27 +0200

    ready to deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net=:${PORT} --cached-only server.js --port=${PORT}
+web: deno run --allow-net --allow-read --cached-only server.js --port=${PORT}
--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 import { listenAndServe } from 'https://deno.land/std/http/server.ts';
-// import * as flags from 'https://deno.land/std/flags/mod.ts';
+import { serveFile } from 'https://deno.land/std@0.58.0/http/file_server.ts';
 import { acceptWebSocket, acceptable } from 'https://deno.land/std/ws/mod.ts';
-// import { Application } from "https://deno.land/x/oak/mod.ts";
-
+import { parse } from 'https://deno.land/std@0.59.0/flags/mod.ts'
 import chat from './chat.js';
 
-// const { args, exit } = Deno;
-// const DEFAULT_PORT = 3000;
-// const argPort = flags.parse(args).port;
-// const port = argPort ? Number(argPort) : DEFAULT_PORT;
-
-// if (isNaN(port)) {
-//     console.log('port is not a number');
-    
-//     exit(1)
-// } 
+const PORT = parse(Deno.args).port ? parseInt(parse(Deno.args).port) : 3000;
 
 
+listenAndServe({port : PORT}, async (request)=>{
 
-// const app = new Application();
+    const path = `${Deno.cwd()}/public/${modifyUrl(request.url)}`;
+    if (await fileExist(path)) {
+        const content = await serveFile(request,path);
+        request.respond(content);
+        return;
+    }
 
-
-// app.use(async (context) => {
-//     await send(context, context.request.url.pathname, {
-//       root: `${Deno.cwd()}/public`,
-//       index: "chat.html",
-//     });
-// });
-
-
-listenAndServe({port}, async (request)=>{
     if (acceptable(request))
         acceptWebSocket({
             conn: request.conn,
@@ -38,7 +24,28 @@ listenAndServe({port}, async (request)=>{
             headers: request.headers
         })
         .then(chat);
-    // app.listen({port})
 })
 
-console.log(`Server started on port 3000`);
+
+async function fileExist(path) {
+    try {
+        const stats = await Deno.lstat(path);
+        return stats && stats.isFile;
+    } catch (error) {
+        if (error && error instanceof Deno.errors.NotFound) {
+            return false;
+        } else {
+            throw error;
+        }
+    }
+}
+
+function modifyUrl(url) {
+    const position = url.indexOf('?');
+    if (position > -1) {
+        url = url.substring(0,position)
+    }
+    return url;
+}
+
+console.log(`Server started on port ${PORT}`);
